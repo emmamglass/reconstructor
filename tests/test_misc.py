9ded29dc83eef60241a12318d28d54b90507d8f9
Adam Bates <adambates923@gmail.com>
2025-03-24 16:31:25 -0400

    Add test to ensure model can be written to sbml file.

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -4,6 +4,7 @@ belong in any of the other test files.
 """
 
 from copy import deepcopy
+import itertools
 
 import pytest
 import cobra
@@ -33,3 +34,27 @@ def test_time_to_copy_reaction(universal_model: cobra.Model):
     
     avg_time = sum(times) / len(times)
     assert avg_time < 0.1
+
+
+def test_write_sbml(universal_model: cobra.Model, tmp_path):
+    """
+    Tests writing a model to SBML (the final action of the Reconstructor script)
+    that has reactions copied from the universal model.
+    
+    With newer versions of cobrapy, reactions from the universal model are
+    sometimes missing attributes that cause errors when writing the model to
+    SBML. The `_annotation` is added in the Reconstructor script so we add it
+    here as well.
+    """
+    new_model = cobra.Model("test_model")
+
+    # Copy a reaction from the universal model
+    new_model.add_reactions([deepcopy(universal_model.reactions[0])])
+
+    # Add _annotation to all genes, reactions, and metabolites (causes error otherwise)
+    for obj in itertools.chain(new_model.genes, new_model.metabolites, new_model.reactions):
+        obj._annotation = {}
+
+    # Test writing the model
+    model_path = tmp_path / "model.sbml"
+    cobra.io.write_sbml_model(new_model, str(model_path))
